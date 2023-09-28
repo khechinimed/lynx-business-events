6e48d8aaebaec7a917cb89c9561a5d71ff36a34f
khechinimed <khechinibakr20@gmail.com>
2023-09-28 19:34:56 +0200

    removing the login from the / page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+use App\Http\Controllers\EventController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,21 +18,23 @@ use Inertia\Inertia;
 |
 */
 
+Route::resource('events', EventController::class);
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+
 });
